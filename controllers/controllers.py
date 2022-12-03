30f39e18cf6b491d9702548c525b84c5ffde8bf4
anikeenko <dimskay1988@icloud.com>
2022-12-03 23:03:14 +0300

    edit controller

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,11 +1,33 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+from odoo.http import request
 
 
 class ToolsControl(http.Controller):
     @http.route('/tools_control/tools_control', auth='public', crf=False)
     def index(self, **kw):
         return "Hello, world"
+
+    @http.route('/get_tools_control', type="json", auth='public', cors='*', crf=False)
+    def get_all_alert(self):
+        alert_rec = request.env['tools_control.tools_control'].sudo.search([])
+        alerts = []
+        for rec in alert_rec:
+            print('rec.photo', type(rec.photo))
+            vals = {
+                'id': rec.id,
+                'action': rec.action,
+                'date': rec.date,
+                'area': rec.area,
+                'photo': rec.photo,
+            }
+            alerts.append(vals)
+        data = {
+            'status': '200',
+            'message': 'success',
+            'response': alerts
+        }
+        return data
 
     @http.route('/tools_control/tools_control/objects', auth='public', crf=False)
     def list(self, **kw):
