aed3cd7e9bbeb964e9abd2bb72a6848027c4dff6
anikeenko <dimskay1988@icloud.com>
2022-12-05 11:57:56 +0300

    edit008 path api

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -10,7 +10,7 @@ class ToolsControl(http.Controller):
         return "Hello, world"
 
     # @http.route('/api', auth='public', website=False)
-    @http.route('/api', auth='user', website=False, crf=True, cors='*', type='json', methods=['GET', 'POST'])
+    @http.route('/api', auth='public', website=False, crf=False, cors='*', type='json', methods=['GET', 'POST'])
     def all_alerts(self, **kw):
         alert_rec = http.request.env['tools_control.tools_control'].sudo().search([])
         alerts = []
@@ -71,3 +71,52 @@ class ToolsControl(http.Controller):
         return http.request.render('tools_control.object', {
             'object': obj
         })
+
+    @http.route('/api_alert', type='json', auth='user', methods=['POST'], cors='*', csrf=False)
+    def create_employee(self, **kwargs):
+        data = json.loads(request.httprequest.data)
+        employee = request.env['tools_control.tools_control'].sudo().search([('action', '=', data.get('action'))])
+        if employee:
+            return {'message': 'already exists'}
+        else:
+            # Create
+            home_action = request.env['tools_control.tools_control'].sudo().create({
+                'action': 'action',
+                'date': 'date',
+                'area': 'area',
+                'photo': 'photo',
+
+            })
+            if home_action:
+                home_action = home_action.id
+                employee = request.env['tools_control.tools_control'].sudo().create({
+                    'date': data.get('date'),
+                    'area': data.get('area'),
+                    'photo': data.get('photo'),
+                    'home_action.id': home_action
+                })
+                if employee:
+                    employee = employee.id
+            return {'message': 'Employee {} created'.format(employee)}
+
+    @http.route('/create_alert', type="json", auth='public', crf=False)
+    def create_patient(self, **rec):
+        if request.jsonrequest:
+            if rec['action']:
+                vals = {
+                    'action': rec['action'],
+                    'date': rec['date'],
+                    'area': rec['area'],
+                    'photo': rec['photo'],
+                }
+                new_alert = request.env['tools_control.tools_control'].sudo().create(vals)
+                args = {
+                    'meaasge': 'success',
+                    'success': 'True',
+                    'data':
+                        {
+                            'id': new_alert.id,
+                            'action': new_alert.action
+                        },
+                }
+        return args
