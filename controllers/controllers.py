abdb112a363ed982814acf72ad7f5a9688597819
anikeenko <dimskay1988@icloud.com>
2022-12-09 12:41:03 +0300

    add custom_auth

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,10 +1,14 @@
 from odoo import http
 from odoo.http import request
+import requests
+import json
 
 
 class ToolsControl(http.Controller):
-    @http.route('/tools/get_all_alerts', auth='user', type='json', method=['POST', 'OPTIONS'])
+    @http.route('/tools/get_all_alerts', auth='user', type='json')
     def all_alerts(self, **kw):
+        # session_id = http.request.env['ir.http'].sudo().search([])
+        # print(session_id)
         alert_rec = http.request.env['tools_control.tools_control'].sudo().search([])
         alerts = []
         for rec in alert_rec:
@@ -14,7 +18,7 @@ class ToolsControl(http.Controller):
                 'area': rec.area,
                 'photo': rec.photo,
             })
-
+        print()
         return alerts
 
     @http.route('/tools/create_alert', auth='user', type='json')
@@ -34,3 +38,27 @@ class ToolsControl(http.Controller):
     @http.route('/tools/ping', type='json', auth='public')
     def ping(self):
         return {'success': True}
+
+    @http.route('/tools/custom_auth', type='json', auth='public')
+    def get_session_id(self, url, db, login, password):
+        urls = f"{url}/web/session/authenticate"
+        payload = json.dumps({
+            "jsonpc": "2.0",
+            "params": {
+                "db": db,
+                "login": login,
+                "password": password
+            }
+        })
+        headers = {'Content-Type': 'application/json'}
+        try:
+            response = requests.request("GET", urls, headers=headers, data=payload)
+        except ValueError:
+            return ValueError
+        session_id = ((response.headers.get("Set-Cookie").split(';')[0]).split('session_id=')[1])
+        data_response = (response.json())
+        data = {
+            'data': data_response,
+            'session_id': session_id,
+        }
+        return data
